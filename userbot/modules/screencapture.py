6a0e8d1ab7afa56ce1189dfc8ebaebae239d4ff8
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-07-30 21:47:27 +0530

    screencapture (without API)
    Since we already use google chrome for .carbon so......
    Why not ??

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -6,55 +6,65 @@
 # The entire source code is OSSRPL except 'screencapture' which is MPL
 # License: MPL and OSSRPL
 
-""" Userbot module for ScreenshotLayer API """
-
-import os
-from requests import get
-
-from userbot import SCREENSHOT_LAYER_ACCESS_KEY, CMD_HELP
+import io
+import traceback
+from os import remove
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from telethon import events
 from userbot.events import register
+from userbot import GOOGLE_CHROME_BIN, CHROME_DRIVER
 
 
 @register(pattern=r".screencapture (.*)", outgoing=True)
 async def capture(url):
     """ For .screencapture command, capture a website and send the photo. """
     if not url.text[0].isalpha() and url.text[0] not in ("/", "#", "@", "!"):
-        if SCREENSHOT_LAYER_ACCESS_KEY is None:
-            await url.edit(
-                "Need to get an API key from https://screenshotlayer.com/product \nModule stopping!"
-            )
+        if event.fwd_from:
             return
-        await url.edit("Processing ...")
-        sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&\
-            url={}&fullpage={}&format={}&viewport={}"
-        input_str = url.pattern_match.group(1)
-        response_api = get(
-            sample_url.format(
-                SCREENSHOT_LAYER_ACCESS_KEY, input_str, "1", "PNG", "2560x1440"
-            ),
-            stream=True,
-        )
-        content_type = response_api.headers["content-type"]
-        if "image" in content_type:
-            temp_file_name = "screencapture.png"
-            with open(temp_file_name, "wb") as file:
-                for chunk in response_api.iter_content(chunk_size=128):
-                    file.write(chunk)
-            try:
-                await url.client.send_file(
-                    url.chat_id,
-                    temp_file_name,
+        if Config.GOOGLE_CHROME_BIN is None:
+            await event.edit("need to install Google Chrome. Module Stopping.")
+            return
+        await event.edit("Processing ...")
+        start = datetime.now()
+        try:
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--test-type")
+            chrome_options.binary_location = GOOGLE_CHROME_BIN
+            chrome_options.add_argument("--window-size=1920x1080")
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument('--disable-gpu')
+            driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
+            input_str = event.pattern_match.group(1)
+            driver.get(input_str)
+            im_png = driver.get_screenshot_as_png()
+            # saves screenshot of entire page
+            driver.close()
+            message_id = event.message.id
+            if event.reply_to_msg_id:
+                message_id = event.reply_to_msg_id
+            with io.BytesIO(im_png) as out_file:
+                out_file.name = "screencapture.png"
+                await event.client.send_file(
+                    event.chat_id,
+                    out_file,
                     caption=input_str,
                     force_document=True,
-                    reply_to=url.message.reply_to_msg_id,
+                    reply_to=message_id,
+                    allow_cache=False,
+                    silent=True
                 )
-                await url.delete()
-            except BaseException:
-                await url.edit(response_api.text)
-            os.remove(temp_file_name)
-        else:
-            await url.edit(response_api.text)
-
+                remove(out_file.name)
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.edit(f"Completed screencapture Process in {ms} seconds")
+        except Exception:
+            await event.edit(traceback.format_exc())
+        
 CMD_HELP.update({
     "screencapture": ".screencapture <url>\
     \nUsage: Takes a screenshot of a website and sends the screenshot."
